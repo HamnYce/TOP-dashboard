92bdda468c29e6eca80577fbd69d61acb6d4d7a4
HamnYce <hamadboa@gmail.com>
2022-09-07 03:44:24 +0300

    add icons add trending

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,13 @@ let container = roo.item(0);
 for (let i = 0; i < 8; i++) {
   container.appendChild(card.cloneNode(true));
 }
+
+let y = document.getElementsByClassName('account-card');
+card = y.item(0);
+
+let foo = document.getElementsByClassName('trending-cards-container');
+container = foo.item(0);
+
+for (let i = 0; i < 3; i++) {
+  container.appendChild(card.cloneNode(true));
+}
