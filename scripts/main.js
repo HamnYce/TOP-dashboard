cc332ae874e75c0c798b73d9721571f8260cdd36
HamnYce <hamadboa@gmail.com>
2022-09-09 01:36:09 +0300

    add announcement text-overflow ellipsis add trending parts js

--- scripts/main.js
+++ scripts/main.js
@@ -15,5 +15,20 @@ let foo = document.getElementsByClassName('trending-cards-container');
 container = foo.item(0);
 
 for (let i = 0; i < 3; i++) {
-  container.appendChild(card.cloneNode(true));
+  let new_card = card.cloneNode(true);
+  let img = new_card.getElementsByClassName('account-image').item(0);
+  img.style.backgroundImage = `url(assets/profile-pics/profile-${i + 1}.jpeg)`
+
+  container.appendChild(new_card);
 }
+
+let card_parts = document.getElementsByClassName('card-part');
+
+let arr = [...card_parts]
+
+
+arr.forEach(element => {
+  let s = element.getElementsByTagName('p').item(0).innerHTML;
+  // TODO: string array indexing how?
+  element.getElementsByTagName('p').item(0).innerHTML = s.slice(0, 180) + '...';
+});
